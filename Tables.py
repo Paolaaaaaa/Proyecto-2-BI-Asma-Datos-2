21d7e76dfd5d9828d30155c258bf15b112ccb057
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2023-05-29 18:00:41 -0500

    transformación

--- Tables.py
+++ Tables.py
@@ -1,41 +1,40 @@
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Sequence
 
 Base = declarative_base()
 
 class Ubicacion(Base):
     __tablename__ = 'ubicacion'
-    pk_ubicacion = Column(Integer, primary_key=True)
+    pk_ubicacion = Column(Integer, Sequence('ubicacion_seq'),primary_key=True )
     localidad = Column(String)
     municipio = Column(Integer)
 
 class Fecha(Base):
     __tablename__ = 'fecha'
     
-    pk_fecha = Column(Integer, primary_key=True)
+    pk_fecha = Column(Integer, Sequence('fecha_seq'),primary_key=True )
     anio = Column(Integer)
 
 
 class Encuestado(Base):
     __tablename__ = 'encuestado'
     
-    pk_encuestado = Column(Integer, primary_key=True)
+    pk_encuestado = Column(Integer, Sequence('encuestado_seq'),primary_key=True )
     edad = Column(Integer)
     sexo = Column(Integer)
 
 
 class Pregunta(Base):
     __tablename__ = 'pregunta'
-    
-    pk_pregunta = Column(Integer, primary_key=True)
+    pk_pregunta = Column(Integer, Sequence('pregunta_seq'),primary_key=True )
     pregunta = Column(String)
 
 class Respuesta (Base):
     __tablename__ = 'respuesta'
     
-    pk_Respuesta = Column(Integer, primary_key=True)
+    pk_Respuesta = Column(Integer, Sequence('respuesta_seq'),primary_key=True )
     respuesta = Column(String)
-    tipo_respuesta = Column(String)
 
 class hecho_respuesta (Base):
     __tablename__ = 'hecho_respuesta'
--- etl.py
+++ etl.py
@@ -6,6 +6,14 @@ from sqlalchemy import create_engine
 import os 
 import transformation as ts
 import Tables 
+from sqlalchemy.orm import Session
+
+encuestado = {}
+ubicacion = {}
+fecha = {}
+pregunta = {}
+respuesta = {}
+
 pwd = "password"
 uid = "etl"
 
@@ -37,8 +45,13 @@ def extract():
             #query and load save data to dataframe
             df_2021_asma = pd.read_sql_query(f'select * FROM {tbl[0]}', src_conn)        
 
+        print ("\n Comenzando la transformación de DATOS: \n ")
+        df_2021=ts.transform_data2021(df_2021_asma)
+        print ("2021 ha sido transformada \n")
         df_2017=ts.transform_data2017(df_2017_asma)
-        df_2021=ts.transform_data2017(df_2021_asma)
+        print ("df_2017 ha sido transformada \n ")
+  
+
         concat_data = ts.join_df(df_2017,df_2021)
         print (concat_data)
 
@@ -54,11 +67,17 @@ def extract():
 
 
 
-def load(df, tbl):
+def load(df):
     try:
         rows_imported = 0
         engine = create_engine(f'postgresql://{uid}:{pwd}@{server}:5432/'+database)
         Tables.Base.metadata.create_all(engine)
+        create_ubicacion(df, engine)
+        create_fecha(df,engine)
+        create_encuestado(df, engine)
+        create_respuesta(df, engine)
+        create_pregunta(df, engine)
+    
 
         
         
@@ -72,3 +91,76 @@ try:
     extract()
 except Exception as e:
     print("Error while extracting data: " + str(e))
+
+
+
+def create_ubicacion(df, engine):
+    session = Session(bind=engine)
+
+    localidades=df['localidad'].unique()
+    municipio=df['municipio'].unique()
+
+    for i in localidades:
+        for j in municipio:
+            nuevo_dato=Tables.Ubicacion(localidad= i, municipio = j)
+            session.add(nuevo_dato)
+    session.commit()
+
+    session.close()
+
+
+
+def create_fecha(df, engine):
+    session = Session(bind=engine)
+
+    localidades=df['ANIO'].unique()
+
+    for i in localidades:
+            nuevo_dato=Tables.Fecha(anio= i)
+            session.add(nuevo_dato)
+    session.commit()
+
+    session.close()
+
+
+def create_encuestado(df, engine):
+    session = Session(bind=engine)
+    edades=df['edad'].unique()
+    sexos=df['sexo'].unique()
+
+    for i in edades:
+        for j in sexos:
+            nuevo_dato=Tables.Encuestado(edad= i, sexo = j)
+            session.add(nuevo_dato)
+    session.commit()
+
+    session.close()
+
+
+
+def create_pregunta(engine):
+    session = Session(bind=engine)
+
+    pregunta = ['Transmilenio',
+       'SITP', 'buseta_o_colectivo', 'automovil_particular', 'Taxi',
+       'Motocicleta', 'Bicicleta', 'Ruta_escolar', 'A_pie', 'NPCHP18J',
+       'bicitaxi_o_mototaxi', 'caballo', 'otros', 'ANIO', 'no_se_desplaza',
+       'vehículo_patineta_moto_electicos',
+       'particulares_plataformas_o_aplicaciones', 'Bus_intermunicipal']
+    for i in pregunta:
+            nuevo_dato=Tables.Pregunta(pregunta = i)
+            session.add(nuevo_dato)
+    session.commit()
+
+    session.close()
+
+def create_respuesta(engine):
+    session = Session(bind=engine)
+
+    
+    for i in ["1","2","NAN"]:
+            nuevo_dato=Tables.Respuesta(respuesta= i)
+            session.add(nuevo_dato)
+    session.commit()
+
+    session.close()
